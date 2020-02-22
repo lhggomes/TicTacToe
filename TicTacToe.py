c35d16ca2a076de29849148ac064d546d851e00c
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 14:19:02 -0300

    Creating a Tie funciton

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,9 +1,9 @@
 # PROJECT TIC TAC TOE
 
 # Creating a Board for the Game
-board = [" - ", " - ", " - ",
-         " - ", " - ", " - ",
-         " - ", " - ", " - ", ]
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-", ]
 # If Game Still going
 playingGame = True
 # Who on? Or there's a Tie?
@@ -36,9 +36,9 @@ def checkRows():
     # Global Variables
     global playingGame
     # Checking THe Rows
-    row1 = board[0] == board[1] == board[2] != " - "
-    row2 = board[3] == board[4] == board[5] != " - "
-    row3 = board[6] == board[7] == board[8] != " - "
+    row1 = board[0] == board[1] == board[2] != "-"
+    row2 = board[3] == board[4] == board[5] != "-"
+    row3 = board[6] == board[7] == board[8] != "-"
     # If Rows are complete
     if row1 or row2 or row3:
         playingGame = False
@@ -54,9 +54,9 @@ def checkRows():
 
 def checkColumns():
     global playingGame
-    column1 = board[0] == board[3] == board[6] != " - "
-    column2 = board[1] == board[4] == board[7] != " - "
-    column3 = board[2] == board[5] == board[8] != " - "
+    column1 = board[0] == board[3] == board[6] != "-"
+    column2 = board[1] == board[4] == board[7] != "-"
+    column3 = board[2] == board[5] == board[8] != "-"
 
     if column1 or column2 or column3:
         playingGame = False
@@ -74,9 +74,8 @@ def checkColumns():
 def checkDiagonal():
     global playingGame
 
-    diagonal1 = board[0] == board[4] == board[8] != " - "
-    diagonal2 = board[2] == board[4] == board[6] != " - "
-
+    diagonal1 = board[0] == board[4] == board[8] != "-"
+    diagonal2 = board[2] == board[4] == board[6] != "-"
     if diagonal1 or diagonal2:
         playingGame = False
 
@@ -88,24 +87,28 @@ def checkDiagonal():
 
 
 def checkIfTie():
+    global playingGame
+    if "-" not in board:
+        playingGame = False
+
     return
 
 
 def flipPlayer():
     global currentPlayer
-    #Changing the current player between O and X
+    # Changing the current player between O and X
     if currentPlayer == "X":
-       currentPlayer = "O"
+        currentPlayer = "O"
     elif currentPlayer == "O":
         currentPlayer = "X"
-        
+
     return
 
 
 def checkIfGameOver():
     checkForWinner()
     checkIfTie()
-   
+
 
 # Display the Board
 def displayBoard():
@@ -141,5 +144,3 @@ while playingGame:
     # The Game has ended
     if winner == "X" or winner == "O":
         print(winner + " won.")
-   
-        
