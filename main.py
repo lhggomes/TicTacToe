0486c6950fcdc5f0e71dba6143eb5cad46335bf7
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 11:40:51 -0300

    Implementation of checkRows function
    All the code of check functions are made.

--- main.py
+++ main.py
@@ -15,17 +15,44 @@ currentPlayer = "X"
 
 
 def checkForWinner():
+
+    global winner
     rowWinner = checkRows()
     columnWinner = checkColumns()
     diagonalWinner = checkDiagonal()
+    if rowWinner:
+        winner = rowWinner()
+    elif columnWinner:
+        winner = columnWinner()
+    elif diagonalWinner:
+        winner = diagonalWinner()
+    else:
+        winner = None
     return
 
 
 def checkRows():
+    # Global Variables
+    global playingGame
+    # Checking THe Rows
+    row1 = board[0] == board[1] == board[2] != "-"
+    row2 = board[3] == board[4] == board[5] != "-"
+    row3 = board[6] == board[7] == board[8] != "-"
+    # If Rows are complete
+    if row1 or row2 or row3:
+        playingGame = False
+    # Retunr the winner X or O
+    if row1:
+        return board[2]
+    if row2:
+        return board[5]
+    if row3:
+        return board[8]
     return
 
-
 def checkColumns():
+    column1 = board[0] == board[3] == board[6] != "-"
+    column2 = board[1] == board[4] == board[7] != "-"
     return
 
 
