e8c82ac8e1157f4ec0345d9ce00e34d7725f470e
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 14:48:34 -0300

    Final Commit
    Adding comments on the code!
    And Final Commit.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,25 +1,32 @@
 # PROJECT TIC TAC TOE
 
-# Creating a Board for the Game
+# The Board of the Game
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-", ]
-# If Game Still going
+
+# Global Variables
 playingGame = True
-# Who on? Or there's a Tie?
 winner = None
-# Whos turn this it
 currentPlayer = "X"
 
-# Functions to check the status of the game
+# Display the Board
+
+
+def displayBoard():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+# Function to Check the Winners of the Games
 
 
 def checkForWinner():
-
     global winner
     rowWinner = checkRows()
     columnWinner = checkColumns()
     diagonalWinner = checkDiagonal()
+
     if rowWinner:
         winner = rowWinner
     elif columnWinner:
@@ -85,6 +92,8 @@ def checkDiagonal():
         return board[6]
     return
 
+# Function to Check If we Have an Tie
+
 
 def checkIfTie():
     global playingGame
@@ -92,6 +101,8 @@ def checkIfTie():
         playingGame = False
 
     return
+
+# Function to Flip the Players (O to X)
 
 
 def flipPlayer():
@@ -104,20 +115,14 @@ def flipPlayer():
 
     return
 
+# Function to Look after the scores
+
 
 def checkIfGameOver():
     checkForWinner()
     checkIfTie()
 
 
-# Display the Board
-def displayBoard():
-    print(board[0] + "|" + board[1] + "|" + board[2])
-    print(board[3] + "|" + board[4] + "|" + board[5])
-    print(board[6] + "|" + board[7] + "|" + board[8])
-
-
-# Let's play the game!
 displayBoard()
 
 
@@ -125,6 +130,8 @@ def playGame():
     print("The Initial Game!")
     displayBoard()
 # Defining a Function to Handle the Positons
+
+# Choosing the Postion of the user
 
 
 def handleTurn(currentPlayer):
@@ -140,7 +147,7 @@ def handleTurn(currentPlayer):
         if board[position] == "-":
             valid = True
         else:
-             print("You can't go in there. Go again!")
+            print("You can't go in there. Go again!")
 
     board[position] = currentPlayer
     displayBoard()
