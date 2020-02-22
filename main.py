344332dc76e07953301fe6100b6a6e1b8cfa9ef1
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 10:46:32 -0300

    Solve Problems
    Soving problems on the code.

--- main.py
+++ main.py
@@ -1,57 +1,79 @@
 # PROJECT TIC TAC TOE
 
-#GLOBAL VARIABLES 
+# GLOBAL VARIABLES
 
-# Creating a Board for the Game 
+# Creating a Board for the Game
 board = [" - ", " - ", " - ",
          " - ", " - ", " - ",
          " - ", " - ", " - ", ]
-#If Game Still going 
+# If Game Still going
 playingGame = True
 
-#Who on? Or there's a Tie? 
+# Who on? Or there's a Tie?
 winner = None
 
-# Function to Display the board 
+# Whos turn this it
+currentPlayer = "X"
+
+# Function to Display the board
+
+
 def displayBoard():
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
-# Let's play the game! 
+# Let's play the game!
 
 
 def playGame():
     print("The Initial Game!")
     displayBoard()
 
-while playingGame:
-    handleTurn(currentPlayer)
-    checkIfGameOver()
-    flipPlayer()
-
-#Defining a Function to Handle the Positons
-def handleTurn():
+# Defining a Function to Handle the Positons
+def handleTurn(currentPlayer):
     position = input("Choose a positon from 1-9: ")
     position = int(position) - 1
     board[position] = "X"
     displayBoard()
+# While the game still going
+while playingGame:
+    # Handlgin the turn of players
+    handleTurn(currentPlayer)
+    # Check if the game has ended
+    checkIfGameOver()
+
+    # Flip to the other player
+    flipPlayer()
+    # The Game has ended
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elif winner == None:
+        print("Tie")
 # Functions to Check the Status of the game
+
+
 def checkIfGameOver():
     checkIfWin()
     checkIfTie()
+# Checking if we have a winner for the Game
 
-#Checking if we have a winner for the Game
+
 def checkIfWin():
     # Check Rows
     # Check Columns
     # Check Diagonals
     return
+# Checking if we have an Tie!
 
-#Checking if we have an Tie! 
+
 def checkIfTie():
     return
-def flipPlayer(): 
-    return 
+
+
+def flipPlayer():
+    return
+
+
 playGame()
 # check win
 # check rows
