88c2cb150f7e0e26b1ae0af3cf3fe502d81dab0d
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 10:09:38 -0300

    Creating the scope of functions
    Creating the scope of the funciotons of the game.

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
-# PROJECT TIC TAC TOE 
+# PROJECT TIC TAC TOE
 
-# board
+# Creating a Board for the Game 
 board = [" - ", " - ", " - ",
          " - ", " - ", " - ",
          " - ", " - ", " - ", ]
-# display
-
+# Function to Display the board 
 def displayBoard():
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
+# Let's play the game! 
 
-# play game
+
 def playGame():
-    # Display the board for the begining of the game
+    print("The Initial Game!")
     displayBoard()
-    handleTurn()
 
-# Handles turn
+while playingGame:
+    handleTurn(currentPlayer)
+    checkIfGameOver()
+    flipPlayer()
+
+#Defining a Function to Handle the Positons
 def handleTurn():
     position = input("Choose a positon from 1-9: ")
+    position = int(position) - 1
+    board[position] = "X"
+    displayBoard()
+# Functions to Check the Status of the game
+def checkIfGameOver():
+    checkIfWin()
+    checkIfTie()
 
- 
+#Checking if we have a winner for the Game
+def checkIfWin():
+    # Check Rows
+    # Check Columns
+    # Check Diagonals
+    return
+
+#Checking if we have an Tie! 
+def checkIfTie():
+    return
+def flipPlayer(): 
+    return 
 playGame()
 # check win
-    # check rows
-    # check columns
-    # check diagonals
-# check tie
-
-# flip player
+# check rows
+# check columns
+# check diagonals
