715013ab03398f21c88235fa097835c310fc9a81
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 10:57:50 -0300

    Fixing Bug
    Fix the bug problems, erro ok.

--- main.py
+++ main.py
@@ -1,69 +1,22 @@
 # PROJECT TIC TAC TOE
 
-# GLOBAL VARIABLES
-
 # Creating a Board for the Game
 board = [" - ", " - ", " - ",
          " - ", " - ", " - ",
          " - ", " - ", " - ", ]
 # If Game Still going
 playingGame = True
-
 # Who on? Or there's a Tie?
 winner = None
-
 # Whos turn this it
 currentPlayer = "X"
 
-# Function to Display the board
-
-
-def displayBoard():
-    print(board[0] + "|" + board[1] + "|" + board[2])
-    print(board[3] + "|" + board[4] + "|" + board[5])
-    print(board[6] + "|" + board[7] + "|" + board[8])
-# Let's play the game!
-
-
-def playGame():
-    print("The Initial Game!")
-    displayBoard()
-
-# Defining a Function to Handle the Positons
-def handleTurn(currentPlayer):
-    position = input("Choose a positon from 1-9: ")
-    position = int(position) - 1
-    board[position] = "X"
-    displayBoard()
-# While the game still going
-while playingGame:
-    # Handlgin the turn of players
-    handleTurn(currentPlayer)
-    # Check if the game has ended
-    checkIfGameOver()
-
-    # Flip to the other player
-    flipPlayer()
-    # The Game has ended
-    if winner == "X" or winner == "O":
-        print(winner + " won.")
-    elif winner == None:
-        print("Tie")
-# Functions to Check the Status of the game
-
-
-def checkIfGameOver():
-    checkIfWin()
-    checkIfTie()
-# Checking if we have a winner for the Game
+# Functions to check the status of the game
 
 
 def checkIfWin():
-    # Check Rows
-    # Check Columns
-    # Check Diagonals
+
     return
-# Checking if we have an Tie!
 
 
 def checkIfTie():
@@ -74,8 +27,42 @@ def flipPlayer():
     return
 
 
+def checkIfGameOver():
+    checkIfWin()
+    checkIfTie()
+
+
+# Display the Board
+def displayBoard():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
+# Let's play the game!
+
+def playGame():
+    print("The Initial Game!")
+    displayBoard()
+# Defining a Function to Handle the Positons
+
+def handleTurn(currentPlayer):
+    position = input("Choose a positon from 1-9: ")
+    position = int(position) - 1
+    board[position] = "X"
+    displayBoard()
+
+
+# While the game still going
+while playingGame:
+    # Handlgin the turn of players
+    handleTurn(currentPlayer)
+    # Check if the game has ended
+    checkIfGameOver()
+
+    flipPlayer()
+    # The Game has ended
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elif winner == None:
+        print("Tie")
+
 playGame()
-# check win
-# check rows
-# check columns
-# check diagonals
