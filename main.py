af3681e5b7aba825628b33ef0b316e7ec73d41b8
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 09:44:39 -0300

    Configure the boards
    In this pull, I've made some changes on the scruture of the Board, and I've made a function to show the board on the screen for the user.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #
 
-board =  ["-","-","-",
-         "-","-","-",
-          "-","-","-",] 
+board =  [" - "," - "," - ",
+         " - "," - "," - ",
+          " - "," - "," - ",] 
+
+def displayBoard(): 
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
 
 
-
+displayBoard(); 
 
 
 #board 
