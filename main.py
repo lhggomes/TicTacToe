04048aea0e87f839c16262f0d9aadedbc250bdb2
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 09:54:27 -0300

    Function playGame()
    Creating the scope of the function playGame() and formating some areas of the code.

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-#
+# PROJECT TIC TAC TOE 
 
-board =  [" - "," - "," - ",
-         " - "," - "," - ",
-          " - "," - "," - ",] 
+# board
+board = [" - ", " - ", " - ",
+         " - ", " - ", " - ",
+         " - ", " - ", " - ", ]
+# display
 
-def displayBoard(): 
+def displayBoard():
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
 
+# play game
+def playGame():
+    # Display the board for the begining of the game
+    displayBoard()
+    handleTurn()
 
-displayBoard(); 
+# Handles turn
+def handleTurn():
+    position = input("Choose a positon from 1-9: ")
 
+ 
+playGame()
+# check win
+    # check rows
+    # check columns
+    # check diagonals
+# check tie
 
-#board 
-#display 
-#play game 
-#Handles turn 
-#check win 
-    #check rows 
-    #check columns
-    #check diagonals
-#check tie 
-
-#flip player 
-
-
-
+# flip player
