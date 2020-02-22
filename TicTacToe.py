74318c3a731865f0abbd91f9d1751163894bc234
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 13:40:38 -0300

    Adjusting Check Functions
    Change the if statament

--- TicTacToe.py
+++ TicTacToe.py
@@ -21,11 +21,12 @@ def checkForWinner():
     columnWinner = checkColumns()
     diagonalWinner = checkDiagonal()
     if rowWinner:
-        winner = rowWinner()
+        winner = rowWinner
     elif columnWinner:
-        winner = columnWinner()
+        winner = columnWinner
     elif diagonalWinner:
-        winner = diagonalWinner()
+        winner = diagonalWinner
+
     else:
         winner = None
     return
@@ -35,9 +36,9 @@ def checkRows():
     # Global Variables
     global playingGame
     # Checking THe Rows
-    row1 = board[0] == board[1] == board[2] != "-"
-    row2 = board[3] == board[4] == board[5] != "-"
-    row3 = board[6] == board[7] == board[8] != "-"
+    row1 = board[0] == board[1] == board[2] != " - "
+    row2 = board[3] == board[4] == board[5] != " - "
+    row3 = board[6] == board[7] == board[8] != " - "
     # If Rows are complete
     if row1 or row2 or row3:
         playingGame = False
@@ -53,9 +54,9 @@ def checkRows():
 
 def checkColumns():
     global playingGame
-    column1 = board[0] == board[3] == board[6] != "-"
-    column2 = board[1] == board[4] == board[7] != "-"
-    column3 = board[2] == board[5] == board[8] != "-"
+    column1 = board[0] == board[3] == board[6] != " - "
+    column2 = board[1] == board[4] == board[7] != " - "
+    column3 = board[2] == board[5] == board[8] != " - "
 
     if column1 or column2 or column3:
         playingGame = False
@@ -73,8 +74,8 @@ def checkColumns():
 def checkDiagonal():
     global playingGame
 
-    diagonal1 = board[0] == board[4] == board[8] != "-"
-    diagonal2 = board[2] == board[4] == board[6] != "-"
+    diagonal1 = board[0] == board[4] == board[8] != " - "
+    diagonal2 = board[2] == board[4] == board[6] != " - "
 
     if diagonal1 or diagonal2:
         playingGame = False
@@ -97,7 +98,7 @@ def flipPlayer():
 def checkIfGameOver():
     checkForWinner()
     checkIfTie()
-
+   
 
 # Display the Board
 def displayBoard():
@@ -129,7 +130,6 @@ while playingGame:
     handleTurn(currentPlayer)
     # Check if the game has ended
     checkIfGameOver()
-
     flipPlayer()
     # The Game has ended
     if winner == "X" or winner == "O":
